e735e873f98c97d0f35401f860ff3c4863e16314
Danella Kababanda <binaganamiella@gmail.com>
2023-05-24 13:31:39 -0400

    Restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+require 'faker'
+5.times do
+  resto = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample,
+  )
+  puts "Restaurant with id #{resto.id} was created"
+end
+puts "All done!"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
